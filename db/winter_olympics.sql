44809e245972a0906e35e2cbf4d71a2832f11609
Thomas Crines <thomascrines@hotmail.com>
2016-10-22 11:32:41 +0100

    add tables

---
+++ db/winter_olympics.sql
@@ -0,0 +1,40 @@
+DROP TABLE results;
+DROP TABLE disciplines;
+DROP TABLE events;
+DROP TABLE nations;
+DROP TABLE athletes;
+
+CREATE TABLE athletes (
+id SERIAL4 PRIMARY KEY,
+name VARCHAR(255),
+gender VARCHAR(6)
+);
+
+CREATE TABLE nations (
+id SERIAL4 PRIMARY KEY,
+name VARCHAR(255),
+flag_url TEXT,
+code VARCHAR(3),
+athlete_id INT4 REFERENCES athletes(id) ON DELETE CASCADE
+);
+
+CREATE TABLE events (
+id SERIAL4 PRIMARY KEY,
+name VARCHAR(255),
+gender VARCHAR(6)
+);
+
+CREATE TABLE disciplines (
+id SERIAL4 PRIMARY KEY,
+name VARCHAR(255),
+event_id INT4 REFERENCES events(id) ON DELETE CASCADE
+);
+
+CREATE TABLE results (
+id SERIAL4 PRIMARY KEY,
+name VARCHAR(255),
+event_id INT4 REFERENCES events(id) ON DELETE CASCADE,
+gold_id INT4 REFERENCES athletes(id) ON DELETE CASCADE,
+silver_id INT4 REFERENCES athletes(id) ON DELETE CASCADE,
+bronze_id INT4 REFERENCES athletes(id) ON DELETE CASCADE
+);
