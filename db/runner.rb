b50362e5d4309490552830f7c3b59ab6444771b9
Thomas Crines <thomascrines@hotmail.com>
2017-02-02 14:41:21 +0000

    remov e if else

--- db/runner.rb
+++ db/runner.rb
@@ -4,20 +4,19 @@ class SqlRunner
 
   def self.run(sql)  
     
-    if ENV['DATABASE_URL'] == nil
-      uri = URI.parse("http://localhost/winter_olympics")
-    else
+    # if ENV['DATABASE_URL'] == nil
+    #   uri = URI.parse("http://localhost/winter_olympics")
+    # else
       uri = URI.parse(ENV['DATABASE_URL'])    
-    end
+    # end
     
     begin
-      db = PG.connect( dbname: uri.path[1..-1], host: uri.host)
+      db = PG.connect( dbname: uri.path[1..-1], host: uri.host, user: uri.user, password: uri.password)
       result = db.exec ( sql )
 
     ensure 
       db.close
     end
-    
     return result
   end
 
